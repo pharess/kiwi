505195fdf5a31b8afe169774728477a059140a6c
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-04-27 15:23:06 -0700

    Made some updates to test to account for the simplified react component. Also made them more comprehensive.

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,30 +2,70 @@ import React from 'react';
 import expect from 'expect';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from '../dev/peter-test/src/App';
 import PeterTest from '../dev/peter-test/src/PeterTest';
-
 import { shallow } from 'enzyme'; // shallow rendering instead of rendering
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Component: PeterTest', () => {
-
     it('renders without crashing', () => {
         const wrapper = shallow(<PeterTest />);
         expect(wrapper.length).toEqual(1);
     });
 
-});
-
-describe('Component: App', () => {
-
     it('receives a proper integer in times', () => {
-       const wrapper = shallow(<App />);
+        const wrapper = Enzyme.mount(<PeterTest color="red" times="2" />);
         expect(
-           parseInt(
-            wrapper.find('PeterTest').prop('times')
-           ) >= 0
+            typeof(wrapper.prop('times')) == 'string'
+        ).toEqual(true);
+    });
+
+    it('receives the correct integer in times', () => {
+        const wrapper = Enzyme.mount(<PeterTest color="red" times="2" />);
+        expect(
+            parseInt(wrapper.prop('times')) == 2
+        ).toEqual(true);
+    });
+
+    it('receives a proper string in color', () => {
+        const wrapper = Enzyme.mount(<PeterTest color="red" times="2" />);
+        expect(
+            typeof (wrapper.prop('color')) === 'string'
+        ).toEqual(true);
+    });
+
+    it('receives the correct string in color', () => {
+        const wrapper = Enzyme.mount(<PeterTest color="red" times="2" />);
+        expect(
+            String(wrapper.prop('color')) === 'red'
+        ).toEqual(true);
+    });
+
+    it('has correct default state type for color', () => {
+        const wrapper = Enzyme.mount(<PeterTest/>);
+        expect(
+            typeof(wrapper.state('color')) === 'string'
+        ).toEqual(true);
+    });
+
+    it('has correct default state for color', () => {
+        const wrapper = Enzyme.mount(<PeterTest/>);
+        expect(
+            String(wrapper.state('color')) === 'black'
+        ).toEqual(true);
+    });
+
+    it('has correct default state type for times', () => {
+        const wrapper = Enzyme.mount(<PeterTest/>);
+        expect(
+            typeof(wrapper.state('times')) === 'number'
+        ).toEqual(true);
+    });
+
+    it('has correct default state for times', () => {
+        const wrapper = Enzyme.mount(<PeterTest/>);
+        expect(
+            parseInt(wrapper.state('times')) === 1
         ).toEqual(true);
     });
 });
--- __tests__/PeterTest.test.js
+++ __tests__/PeterTest.test.js
@@ -1,15 +1,11 @@
-// PeterTest.react.test.js
 import React from 'react';
-//import PeterTest from '../PeterTest.react';
 import PeterTest from '../dev/peter-test/src/PeterTest';
 import renderer from 'react-test-renderer';
 
-//test('PeterTest works', () => {
 describe('PeterTest component renders the PeterTest correctly', () => {
   it('renders correctly', () => {
     const component = renderer.create(
-        <PeterTest color="red" times="2"/>,
-        //<h1>Hello World</h1>,
+        <PeterTest color="red" times="2"/>
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
