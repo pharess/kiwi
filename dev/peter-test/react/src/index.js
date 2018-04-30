16bbd8edb3bffc75ed7c6d895a3caf47267f5e16
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-04-30 14:22:29 -0700

    Made a super small change to PeterTest component.

--- dev/peter-test/react/src/index.js
+++ dev/peter-test/react/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import PeterTest from "./PeterTest.jsx";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<PeterTest color="purple" times="2"/>, document.getElementById("root"));
+ReactDOM.render(<PeterTest color="seagreen" times="3"/>, document.getElementById("root"));
 registerServiceWorker();
