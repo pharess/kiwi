a1aafd592b5220506f09eeff3e1c461c52d1d733
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-04-30 14:08:31 -0700

    Made a change to reset Codacy

--- dev/peter-test/react/src/index.js
+++ dev/peter-test/react/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import PeterTest from "./PeterTest.jsx";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<PeterTest color="blue" times="2"/>, document.getElementById("root"));
+ReactDOM.render(<PeterTest color="blue" times="3"/>, document.getElementById("root"));
 registerServiceWorker();
