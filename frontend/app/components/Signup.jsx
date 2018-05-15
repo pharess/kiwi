bc9f78dbc4c74f161041a4c0fd20d52f4b1ddbdc
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-05-15 15:32:38 -0700

    Finished updating the UI for login and signup.

--- frontend/app/components/Signup.jsx
+++ frontend/app/components/Signup.jsx
@@ -1,251 +1,181 @@
 // - Import react components
-import React,{Component} from 'react'
-import {connect} from 'react-redux'
-import {push} from 'react-router-redux'
-import {NavLink, withRouter} from 'react-router-dom'
-import Paper from 'material-ui/Paper'
-import TextField from 'material-ui/TextField'
-import RaisedButton from 'material-ui/RaisedButton'
-import FlatButton from 'material-ui/FlatButton'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import { NavLink, withRouter } from 'react-router-dom';
+import TextField from 'material-ui/TextField';
 
 // - Import actions
-import *  as authorizeActions from 'authorizeActions'
-import * as globalActions from 'globalActions'
+import *  as authorizeActions from 'authorizeActions';
+import * as globalActions from 'globalActions';
 
 // - Import app API
-import StringAPI from 'StringAPI'
-
+import StringAPI from 'StringAPI';
 
 // - Create Signup componet class
 export class Signup extends Component {
 
-  /**
-   * Component constructor
-   * @param  {object} props is an object properties of component
-   */
-  constructor(props){
-    super(props);
+    /**
+     * Component constructor
+     * @param  {object} props is an object properties of component
+     */
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      fullNameInput: '',
-      fullNameInputError: '',
-      emailInput: '',
-      emailInputError: '',
-      passwordInput: '',
-      passwordInputError: '',
-      confirmInput: '',
-      confirmInputError: ''
-
-    }
-    // Binding function to `this`
-    this.handleForm = this.handleForm.bind(this)
-
-  }
-
-  /**
-   * Handle data on input change
-   * @param  {event} evt is an event of inputs of element on change
-   */
-  handleInputChange = (evt) => {
-   const target = evt.target
-   const value = target.type === 'checkbox' ? target.checked : target.value
-   const name = target.name
-   this.setState({
-     [name]: value
-   })
-
-
-   switch (name) {
-     case 'fullNameInput':
-        this.setState({
-          fullNameInputError: '',
-        })
-        break
-      case 'emailInput':
-       this.setState({
-         emailInputError: ''
-       })
-       break
-     case 'passwordInput':
-      this.setState({
-        confirmInputError: '',
-        passwordInputError: ''
-      })
-      break
-    case 'confirmInput':
-     this.setState({
-       confirmInputError: '',
-       passwordInputError: ''
-     })
-     break
-     case 'checkInput':
-      this.setState({
-        checkInputError: ''
-      })
-       break;
-     default:
-
-
-   }
- }
-
-  /**
-   * Handle register form
-   */
-  handleForm = () => {
-
-   const {fullNameInput, emailInput, passwordInput, confirmInput} = this.state
-   const {register} = this.props
-
-    var error = false
-    
-     // Validate full name
-     let fullNameCheck = fullNameInput.trim().toLowerCase()
-
-     if (fullNameCheck.indexOf('test') > -1
-      || fullNameCheck.indexOf('demo') > -1
-      || fullNameCheck.indexOf('asd') > -1
-      || fullNameCheck.length < 4) {
-      this.setState({
-        fullNameInputError: 'Please enter a valid name.'
-      })
-      error = true
+        this.state = {
+            fullNameInput: '',
+            fullNameInputError: '',
+            emailInput: '',
+            emailInputError: '',
+            passwordInput: '',
+            passwordInputError: '',
+            confirmInput: '',
+            confirmInputError: ''
+        }
     }
 
-     /* Validate email*/
-     if (!StringAPI.isValidEmail(emailInput)) {
-      this.setState({
-        emailInputError: 'Please enter a valid email.'
-      })
-      error = true
+    /**
+     * Handle data on input change
+     * @param  {event} event is an event of inputs of element on change
+     */
+    handleInputChange = (event) => {
+        const target = evt.target;
+        const name = target.name;
 
+        this.setState({ [name]: target.value });
+
+        if (name === 'fullNameInput') {
+            this.setState({ fullNameInputError: '' });
+        }
+
+        else if (name === 'emailInput') {
+            this.setState({ emailInputError: '' });
+        }
+
+        else if (name === 'passwordInput') {
+            this.setState({
+                confirmInputError: '',
+                passwordInputError: ''
+            });
+        }
+
+        else if (name === 'confirmInput') {
+            this.setState({
+                confirmInputError: '',
+                passwordInputError: ''
+            });
+        }
+
+        else if (name === 'checkInput') {
+            this.setState({ checkInputError: '' });
+        }
     }
-    
-    /* Check password */
-     if (passwordInput === '') {
-       this.setState({
-         passwordInputError: 'This field is required.'
-       })
-       error = true
 
-     }
-     if (confirmInput === '') {
-       this.setState({
-         confirmInputError: 'This field is required.'
-       })
-       error = true
+    handleForm = () => {
+        const { fullNameInput, emailInput, passwordInput, confirmInput } = this.state;
 
-     }
-     else if(confirmInput !== passwordInput){
-       this.setState({
-         passwordInputError: 'This field sould be equal to confirm password.',
-         confirmInputError: 'This field sould be equal to password.'
-       })
-       error = true
+        // Validate full name
+        let fullNameCheck = fullNameInput.trim().toLowerCase()
 
-     }
-     if (!error) {
-       register({
-          email: emailInput,
-          password: passwordInput,
-          fullName: fullNameInput
-        })
-     }
+        if (fullNameCheck.length < 2) {
+            this.setState({ fullNameInputError: 'Please enter a valid name.' });
+        }
 
-  }
+        /* Validate email*/
+        if (!StringAPI.isValidEmail(emailInput)) {
+            this.setState({ emailInputError: 'Please enter a valid email.'});
+        }
 
-/**
- * Reneder component DOM
- * @return {react element} return the DOM which rendered by component
- */
-  render() {
-    const paperStyle = {
-  minHeight: 500,
-  width: 450,
-  textAlign: 'center',
-  display: 'block',
-  margin: "auto"
-};
+        /* Check password */
+        if (passwordInput === '') {
+            this.setState({ passwordInputError: 'This field is required.' });
+        }
 
-    return (
+        if (confirmInput === '') {
+            this.setState({ confirmInputError: 'This field is required.' });
+        }
 
-  <div>
+        else if (confirmInput !== passwordInput) {
+            this.setState({
+                passwordInputError: 'This field sould be equal to confirm password.',
+                confirmInputError: 'This field sould be equal to password.'
+            });
+        }
 
-      <h1 style={{
-        textAlign: "center",
-        padding:"20px",
-        fontSize: "30px",
-        fontWeight: 500,
-        lineHeight: "32px",
-              margin: "auto",
-              color: "rgba(138, 148, 138, 0.2)"
-            }}>Green</h1>
+        else {
+            this.props.register({
+                email: emailInput,
+                password: passwordInput,
+                fullName: fullNameInput
+            })
+        }
+    }
 
-          <div className="animate-bottom">
-   <Paper style={paperStyle}  zDepth={1} rounded={false} >
-     <div style={{padding: "48px 40px 36px"}}>
-     <div style={{paddingLeft: "40px",
-    paddingRight: "40px"}}>
+    /**
+     * Reneder component DOM
+     * @return {react element} return the DOM which rendered by component
+     */
+    render() {
+        return (
+            <div style={{height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <div style={{backgroundColor: 'white', width: '450px', textAlign: 'center', borderRadius: '10px'}}>
+                    <h1>Sign up</h1>
 
-     <h2 style={{
-         textAlign: "left",
-             paddingTop: "16px",
-             fontSize: "24px",
-             fontWeight: 400,
-             lineHeight: "32px",
-             margin: 0}}>Sign up</h2>
-         </div>
+                    <TextField
+                        onChange={this.handleInputChange}
+                        errorText={this.state.fullNameInputError}
+                        name="fullNameInput"
+                        floatingLabelStyle={{ fontSize: "15px" }}
+                        floatingLabelText="Full Name"
+                        type="text"
+                    />
+                    
+                    <br />
 
-     <TextField
-       onChange={this.handleInputChange}
-       errorText={this.state.fullNameInputError}
-       name="fullNameInput"
-       floatingLabelStyle={{fontSize:"15px"}}
-      floatingLabelText="Full Name"
-      type="text"
-    /><br />
-    <TextField
-      onChange={this.handleInputChange}
-      errorText={this.state.emailInputError}
-      name="emailInput"
-      floatingLabelStyle={{fontSize:"15px"}}
-     floatingLabelText="Email"
-     type="email"
-   /><br />
-   <TextField
-     onChange={this.handleInputChange}
-     errorText={this.state.passwordInputError }
-     name="passwordInput"
-     floatingLabelStyle={{fontSize:"15px"}}
-    floatingLabelText="Password"
-    type="password"
-  /><br />
-  <TextField
-    onChange={this.handleInputChange}
-    errorText={this.state.confirmInputError }
-    name="confirmInput"
-    floatingLabelStyle={{fontSize:"15px"}}
-   floatingLabelText="Confirm Password"
-   type="password"
- /><br />
-<br />
-  <div className="signup__button-box">
-    <div>
-      <FlatButton label="Login" onClick={this.props.loginPage} />
-    </div>
-    <div>
-      <RaisedButton label="Create" primary={true} onClick={this.handleForm}/>
+                    <TextField
+                        onChange={this.handleInputChange}
+                        errorText={this.state.emailInputError}
+                        name="emailInput"
+                        floatingLabelStyle={{ fontSize: "15px" }}
+                        floatingLabelText="Email"
+                        type="email"
+                    />
 
-    </div>
-  </div>
+                    <br />
 
-</div>
-   </Paper>
- </div>
-  </div>
+                    <TextField
+                        onChange={this.handleInputChange}
+                        errorText={this.state.passwordInputError}
+                        name="passwordInput"
+                        floatingLabelStyle={{ fontSize: "15px" }}
+                        floatingLabelText="Password"
+                        type="password"
+                    />
+                    
+                    <br />
 
-    )
-  }
+                    <TextField
+                        onChange={this.handleInputChange}
+                        errorText={this.state.confirmInputError}
+                        name="confirmInput"
+                        floatingLabelStyle={{ fontSize: "15px" }}
+                        floatingLabelText="Confirm Password"
+                        type="password"
+                    />
+
+                    <br />
+
+                    <div style={{display: 'flex', width: '100%', flexDirection: 'column'}}>
+                        <div style={{display: 'flex', outline: 'none', cursor: 'pointer', alignItems: 'center', justifyContent: 'center', height: '50px', backgroundColor: 'white', color: 'black', marginRight: '31px'}} label="Return to Login screen" onClick={this.props.loginPage}>
+                            <svg style={{marginRight: '10px'}} width="21" height="17" xmlns="http://www.w3.org/2000/svg"><path d="M7.723.67A.966.966 0 0 1 9.09 2.039L3.644 7.484l16.087.04a.978.978 0 0 1 .973.975.966.966 0 0 1-.968.97L3.56 9.426l5.489 5.488a.973.973 0 0 1 .004 1.375.973.973 0 0 1-1.375-.005L.583 9.19A.973.973 0 0 1 .58 7.815L7.723.67z" fill="#A2A2A2"/></svg>
+                            Return to login page
+                        </div>
+                        <div style={{display: 'flex', outline: 'none', cursor: 'pointer', alignItems: 'center', justifyContent: 'center', height: '50px', backgroundColor: '#9013FE', color: 'white', borderRadius: '0 0 10px 10px'}} label="Create Account" onClick={this.handleForm}>Create</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 
@@ -255,18 +185,18 @@ export class Signup extends Component {
  * @param  {object} ownProps is the props belong to component
  * @return {object}          props of component
  */
-const mapDispatchToProps = (dispatch,ownProps) => {
-  return {
-    showError: (message) => {
-      dispatch(action(types.SHOW_ERROR_MESSAGE_GLOBAL)(error.message))
-    },
-    register: (data) => {
-      dispatch(authorizeActions.dbSignup(data))
-    },
-    loginPage: () => {
-      dispatch(push("/login"))
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        showError: (message) => {
+            dispatch(action(types.SHOW_ERROR_MESSAGE_GLOBAL)(error.message))
+        },
+        register: (data) => {
+            dispatch(authorizeActions.dbSignup(data))
+        },
+        loginPage: () => {
+            dispatch(push("/login"))
+        }
     }
-  }
 }
 
 /**
@@ -275,11 +205,11 @@ const mapDispatchToProps = (dispatch,ownProps) => {
  * @param  {object} ownProps is the props belong to component
  * @return {object}          props of component
  */
-const mapStateToProps = (state,ownProps) => {
-  return{
+const mapStateToProps = (state, ownProps) => {
+    return {
 
-  }
+    }
 }
 
 // - Connect component to redux store
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(Signup))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Signup))
