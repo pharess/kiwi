de210e4d2239baa1af3efb58515a2395a150d1c2
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-05-15 12:40:03 -0700

    Updated some of the components to reduce file size and clean up the code.

--- frontend/app/app.jsx
+++ frontend/app/app.jsx
@@ -1,28 +1,28 @@
 // Import external components refrence
-import React from 'react'
-import ReactDOM from 'react-dom'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import { cyan500 } from 'material-ui/styles/colors'
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { cyan500 } from 'material-ui/styles/colors';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-import { Provider } from 'react-redux'
-import store, { history } from 'configureStore'
-import { ConnectedRouter } from 'react-router-redux'
+import { Provider } from 'react-redux';
+import store, { history } from 'configureStore';
+import { ConnectedRouter } from 'react-router-redux';
 
 // - Import app components
-import Master from 'Master'
+import Master from 'Master';
 
 // - Impport actions
 
 // Set default data
 store.subscribe(() => {
-})
+});
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin()
+injectTapEventPlugin();
 
 // This replaces the textColor value on the palette
 // and then update the keys for each component that depends on it.
@@ -30,13 +30,13 @@ injectTapEventPlugin()
 const muiTheme = getMuiTheme({
 
 
-})
+});
 
 
 
 // App css
-require('applicationStyles')
-const supportsHistory = 'pushState' in window.history
+require('applicationStyles');
+const supportsHistory = 'pushState' in window.history;
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -47,4 +47,4 @@ ReactDOM.render(
 		</ConnectedRouter>
 	</Provider>,
 	document.getElementById('app')
-)
+);
--- frontend/app/components/Login.jsx
+++ frontend/app/components/Login.jsx
@@ -1,171 +1,121 @@
-// - Import external components
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { NavLink, withRouter } from 'react-router-dom'
-import { push } from 'react-router-redux'
-import Paper from 'material-ui/Paper'
-import TextField from 'material-ui/TextField'
-import RaisedButton from 'material-ui/RaisedButton'
-import FlatButton from 'material-ui/FlatButton'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { NavLink, withRouter } from 'react-router-dom';
+import { push } from 'react-router-redux';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 
 // - Import actions
-import * as authorizeActions from 'authorizeActions'
+import * as authorizeActions from 'authorizeActions';
 
-
-// - Create Login component class
 export class Login extends Component {
 
-  /**
-   * Component constructor
-   * @param  {object} props is an object properties of component
-   */
-  constructor(props) {
-    super(props);
+    /**
+     * Component constructor
+     * @param  {object} props is an object properties of component
+     */
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      emailInput: '',
-      emailInputError: '',
-      passwordInput: '',
-      passwordInputError: '',
-
-
-    }
-    // Binding function to `this`
-    this.handleForm = this.handleForm.bind(this)
-
-  }
-
-  /**
-   * Handle data on input change
-   * @param  {event} evt is an event of inputs of element on change
-   */
-  handleInputChange = (evt) => {
-    const target = evt.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value
-    });
-
-
-    switch (name) {
-      case 'emailInput':
-        this.setState({
-          emailInputError: ''
-        })
-        break
-      case 'passwordInput':
-        this.setState({
-          confirmInputError: '',
-          passwordInputError: ''
-        })
-
-        break;
-      default:
-
-    }
-  }
-
-  /**
-   * Handle register form
-   */
-  handleForm = () => {
-
-    var error = false
-    if (this.state.emailInput === '') {
-      this.setState({
-        emailInputError: 'This field is required'
-      })
-      error = true
-
-    }
-    if (this.state.passwordInput === '') {
-      this.setState({
-        passwordInputError: 'This field is required'
-      })
-      error = true
-
+        this.state = {
+            emailInput: '',
+            emailInputError: '',
+            passwordInput: '',
+            passwordInputError: ''
+        };
     }
 
-    if (!error) {
-      this.props.login(
-        this.state.emailInput,
-        this.state.passwordInput
-      )
+    /**
+     * Handle data on input change
+     * @param  {event} evt is an event of inputs of element on change
+     */
+    handleInputChange = (evt) => {
+        const target = evt.target;
+        const name = target.name;
+
+        this.setState({ [name]: target.value });
+
+        // Clear text error fields.
+        if (name === 'emailInput') {
+            this.setState({ emailInputError: '' });
+        }
+
+        else if (name === 'passwordInput') {
+            this.setState({ passwordInputError: '' });
+        }
     }
 
-  }
+    handleForm = () => {
+        if (this.state.emailInput === '') {
+            this.setState({ emailInputError: 'This field is required' });
+        }
+
+        else if (this.state.passwordInput === '') {
+            this.setState({ passwordInputError: 'This field is required' });
+        }
+
+        else {
+            this.props.login(this.state.emailInput, this.state.passwordInput);
+        }
+    }
 
 
-  /**
-   * Reneder component DOM
-   * @return {react element} return the DOM which rendered by component
-   */
-  render() {
+    /**
+     * Reneder component DOM
+     * @return {react element} return the DOM which rendered by component
+     */
+    render() {
+        return (
+            <form style={{height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                {/* <div className="animate-bottom"> */}
+                    <div style={{backgroundColor: 'white', width: '450px', margin: '20px', textAlign: 'center', display: 'flex', minHeight: '370px', borderRadius: '10px', justifyContent: 'center', alignItems: 'center'}}>
+                        <div style={{ padding: "48px 40px 36px" }}>
+                            <div style={{paddingLeft: "40px", paddingRight: "40px"}}>
+                                <h2 style={{
+                                    textAlign: "left",
+                                    paddingTop: "16px",
+                                    fontSize: "24px",
+                                    fontWeight: 400,
+                                    lineHeight: "32px",
+                                    margin: 0
+                                }}>Sign in</h2>
+                            </div>
 
-    const paperStyle = {
-      minHeight: 370,
-      width: 450,
-      margin: 20,
-      textAlign: 'center',
-      display: 'block',
-      margin: "auto"
-    };
-    return (
-      <form>
-        <div className="animate-bottom">
-          <Paper style={paperStyle} zDepth={1} rounded={false} >
-            <div style={{ padding: "48px 40px 36px" }}>
-              <div style={{
-                paddingLeft: "40px",
-                paddingRight: "40px"
-              }}>
-
-                <h2 style={{
-                  textAlign: "left",
-                  paddingTop: "16px",
-                  fontSize: "24px",
-                  fontWeight: 400,
-                  lineHeight: "32px",
-                  margin: 0
-                }}>Sign in</h2>
-              </div>
-
-              <TextField
-                onChange={this.handleInputChange}
-                errorText={this.state.emailInputError}
-                name="emailInput"
-                floatingLabelStyle={{ fontSize: "15px" }}
-                floatingLabelText="Email"
-                type="email"
-                tabIndex={1}
-              /><br />
-              <TextField
-                onChange={this.handleInputChange}
-                errorText={this.state.passwordInputError}
-                name="passwordInput"
-                floatingLabelStyle={{ fontSize: "15px" }}
-                floatingLabelText="Password"
-                type="password"
-                tabIndex={2}
-              /><br />
-              <br />
-              <br />
-              <div className="login__button-box">
-                <div>
-                  <FlatButton label="Create an account" onClick={this.props.signupPage} tabIndex={4} />
-                </div>
-                <div >
-                  <RaisedButton label="Login" primary={true} onClick={this.handleForm} tabIndex={3} />
-                </div>
-              </div>
-
-            </div>
-          </Paper>
-        </div>
-      </form>
-    )
-  }
+                            <TextField
+                                onChange={this.handleInputChange}
+                                errorText={this.state.emailInputError}
+                                name="emailInput"
+                                floatingLabelStyle={{ fontSize: "15px" }}
+                                floatingLabelText="Email"
+                                type="email"
+                                tabIndex={1}
+                            /><br />
+                            <TextField
+                                onChange={this.handleInputChange}
+                                errorText={this.state.passwordInputError}
+                                name="passwordInput"
+                                floatingLabelStyle={{ fontSize: "15px" }}
+                                floatingLabelText="Password"
+                                type="password"
+                                tabIndex={2}
+                            /><br />
+                            <br />
+                            <br />
+                            <div className="login__button-box">
+                                <div>
+                                    <FlatButton label="Create an account" onClick={this.props.signupPage} tabIndex={4} />
+                                </div>
+                                <div >
+                                    <RaisedButton label="Login" primary={true} onClick={this.handleForm} tabIndex={3} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                {/* </div> */}
+            </form>
+        )
+    }
 }
 
 /**
@@ -175,14 +125,14 @@ export class Login extends Component {
  * @return {object}          props of component
  */
 const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    login: (username, password) => {
-      dispatch(authorizeActions.dbLogin(username, password))
-    },
-    signupPage: () => {
-      dispatch(push("/signup"))
+    return {
+        login: (username, password) => {
+            dispatch(authorizeActions.dbLogin(username, password))
+        },
+        signupPage: () => {
+            dispatch(push("/signup"))
+        }
     }
-  }
 }
 
 /**
@@ -192,9 +142,9 @@ const mapDispatchToProps = (dispatch, ownProps) => {
  * @return {object}          props of component
  */
 const mapStateToProps = (state, ownProps) => {
-  return {
+    return {
 
-  }
+    }
 }
 
 // - Connect component to redux store
