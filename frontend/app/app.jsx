970a1d01c1db3386444b6cb5c967ba9084d4bcc7
Aykan Fonseca <aykanfonseca5@gmail.com>
2018-05-17 11:28:24 -0700

    asdasd

--- frontend/app/app.jsx
+++ frontend/app/app.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { cyan500 } from 'material-ui/styles/colors';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -14,25 +13,9 @@ import { ConnectedRouter } from 'react-router-redux';
 // - Import app components
 import Master from 'Master';
 
-// - Impport actions
-
-// Set default data
-store.subscribe(() => {
-});
-
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
-
-// This replaces the textColor value on the palette
-// and then update the keys for each component that depends on it.
-// More on Colors: http://www.material-ui.com/#/customization/colors
-const muiTheme = getMuiTheme({
-
-
-});
-
-
 
 // App css
 require('applicationStyles');
