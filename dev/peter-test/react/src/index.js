ffa345937138c8be0c2c9cbd980207922ac8371d
xamgeis <max.t.geislinger@gmail.com>
2018-05-21 13:08:49 -0700

    Renamed Frontend to src/ . Removed peter-testcomponent directory

--- dev/peter-test/react/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PeterTest from "./PeterTest.jsx";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<PeterTest color="blue" times="5"/>, document.getElementById("root"));
-registerServiceWorker();
